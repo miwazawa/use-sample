3a0d4aac1d4967f3c2bdec33b15406096ace3144
miwazawa <miyatomo441@gmail.com>
2024-05-20 02:21:10 +0900

    feat: fetch to pokeapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense, use, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const getPokemonData = async (pokemon: string) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  return response.json();
+};
+
+const Pokemon = () => {
+  const [promise, setPromise] = useState(() => getPokemonData("pikachu"));
+
+  const fetchPokemon = () => {
+    const randomPokemonId = Math.floor(Math.random() * 151) + 1;
+    setPromise(getPokemonData(`${randomPokemonId}`));
+  };
+
+  const pokemonData = use(promise);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img
+        src={pokemonData.sprites.other["official-artwork"].front_default}
+        alt={pokemonData.species.name}
+      />
+      <p>{pokemonData.species.name}</p>
+      <button onClick={fetchPokemon}>fetch</button>
     </>
-  )
+  );
+};
+
+function App() {
+  return (
+    <>
+      <Suspense fallback={<div>loading...</div>}>
+        <Pokemon />
+      </Suspense>
+    </>
+  );
 }
 
-export default App
+export default App;
